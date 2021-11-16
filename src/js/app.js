c3a3013c88fcc2e97d9ad7d64d1d85714f5a6c1a
Iliyan <ili.2@abv.bg>
2021-11-16 22:24:38 +0200

    Nineth_commit

--- src/js/app.js
+++ src/js/app.js
@@ -8,12 +8,14 @@ window.addEventListener("DOMContentLoaded", () => {
     alert("💣");
   });
   
+  body.addEventListener("click", () => {
   var article = document.createElement("article");
   article.classList.add("message");
-  article.inner = "Text";
+  article.innerText = "Text";
   document.body.appendChild(article);
   document.body.appendChild(article);
   document.body.appendChild(article);
   document.body.appendChild(article);
   document.body.appendChild(article);
+  });
 });
